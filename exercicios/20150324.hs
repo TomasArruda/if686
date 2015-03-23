cb44c6776632706aa599efd73e187ce80560bac3
tomasarruda <tomas.arruda@gmail.com>
2015-03-23 09:52:43 -0300

    exercicio

--- exercicios/20150324.hs
+++ exercicios/20150324.hs
@@ -1,6 +1,17 @@
+bigger:: [Int] -> Int -> [Int]
+bigger [] p = []
+bigger (a:as) p
+	| a >=  p = (a: bigger as p)
+	|otherwise = bigger as p
+
+smaller:: [Int] -> Int -> [Int]
+smaller [] p = []
+smaller (a:as) p
+	| a <  p = (a: smaller as p)
+	|otherwise = smaller as p
+
 quicksort:: [Int] -> [Int]
 quicksort [] = []
-quicksort (pivot:list)
-	|pivot > head list = (head list):(quicksort (pivot:(tail list)))
-	|otherwise = pivot:(quicksort (tail list))
+quicksort (pivot:list) = (quicksort (smaller list pivot))++[pivot]++ (quicksort (bigger list pivot))
+
 	
