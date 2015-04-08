067c562c7d8497a8a665ffbe69f24153fb048dc9
tomasarruda <tomas.arruda@gmail.com>
2015-04-08 10:39:50 -0300

    trabalho

--- 20150409.hs
+++ 20150409.hs
@@ -2,32 +2,71 @@
 --Questão 1
 data Graph a = Graph [a] [(a, a, Int)] deriving (Show, Eq, Ord)
 
-quicksort :: [Int] -> [Int]
+quicksort :: (Ord t) => [t] -> [t]
 quicksort [] = []
-quicksort (pivot:list) = (quicksort (smaller list pivot))++[pivot]++ (quicksort (bigger list pivot))
+quicksort (p:ls) = quicksort([e1|e1<-ls, e1<=p])++[p]++quicksort([e2|e2<-ls, e2>p])
 
-quicksortTupla :: [Int] -> [Int]
+quicksortTupla :: (Ord t) => [(t,t,Int)] -> [(t,t,Int)]
 quicksortTupla [] = []
-quicksortTupla (pivot:list) = (quicksortTupla (smaller list pivot))++[pivot]++ (quicksortTupla (bigger list pivot))
+quicksortTupla (p:ls) = quicksortTupla([r|r<-ls, r<=p])++[p]++quicksortTupla([t|t<-ls, t>p])
 
-compareList :: [a] -> [a] -> Bool
+compareList :: (Ord a) => [a] -> [a] -> Bool
 compareList [] [] = True
 compareList (a:as) (b:bs)
 	|a == b = compareList as bs
 	|otherwise = False
 
-compareListTupla :: [(a,a,Int)] -> [(a,a,Int)] -> Bool
-compareList [] [] = True
-compareList ((a1,a2,p1):as) ((b1,b2,p2):bs)
+compareListTupla :: (Ord a) => [(a,a,Int)] -> [(a,a,Int)] -> Bool
+compareListTupla [] [] = True
+compareListTupla ((a1,a2,p1):as) ((b1,b2,p2):bs)
 	| a1 == b1 && a2 == b2 && p1 == p2 = compareListTupla as bs
 	|otherwise = False
 
-equals :: (Graph a) -> (Graph a) -> Bool
+equals :: (Ord a) => (Graph a) -> (Graph a) -> Bool
 equals (Graph l1 la1) (Graph l2 la2)
-	|compareList (quicksort l1) (quicksort l2) == True = compareListTupla la1 la2
+	|compareList (quicksort l1) (quicksort l2) == True = compareListTupla (quicksort la1) (quicksort la2)
 	|otherwise = False
+
 
 --Questão 2
 
+contens::(Eq a) => [a] -> a -> Bool
+contens [] a = False
+contens (l:ls) a
+	| l == a = True
+	| otherwise = contens ls a
+
+getAdjacent :: (Eq a) => [(a,a,Int)] -> a -> [a]
+getAdjacent [] a = []
+getAdjacent ((x,y,p):ls) a
+	|x == a = (y:getAdjacent ls a)
+	|y == a = (x:getAdjacent ls a)
+	|otherwise =  getAdjacent ls a
+
+	
+excludePassBy :: (Eq a) => [a] -> a -> [a]
+excludePassBy [] a = []
+excludePassBy (a:as) b
+	| a == b = excludePassBy as b
+	|otherwise = (a:excludePassBy as b)
+
+orList :: [Bool] -> Bool
+orList [] = False
+orList (x:xs) = x || (orList xs)
+
+-- o grafo, lista de adjacencias ou [nó inicial], lista de nós que serão excluidos quando passados, nó destino
+findBool :: (Eq a) => Graph a -> [a] -> [a] -> a -> [Bool]
+findBool _ _ [] _ = [False]
+findBool _ [] _ _ = [False]
+findBool (Graph l ls ) (x:xs) ys a 
+	| adjacentes == [] = findBool (Graph l ls ) xs ys a
+	| contens (adjacentes) a == True = [True]
+	| contens ys x == False =  findBool (Graph l ls ) xs ys a
+	| otherwise = (False:findBool (Graph l ls) adjacentes (excludePassBy ys x) a)
+	where
+		adjacentes = getAdjacent ls x
+
+find :: (Eq a) => Graph a -> a -> a -> Bool
+find (Graph l ls ) ini cheg = orList (findBool (Graph l ls) [ini] l cheg)
 
 --Exercícios
